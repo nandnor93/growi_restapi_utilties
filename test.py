19eeaf5f751313f90f266009625e6185d4d356cf
Kota Ando <ando@ist.hokudai.ac.jp>
2022-09-11 16:26:07 +0900

    Rearrange into a class

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
+from email.mime import base
 import pprint
 from utils import *
 
 if __name__ == '__main__':
     base_url = 'https://<wiki domain>'
     api_token = 'API Token'
+    growi = GrowiAPI(base_url=base_url, api_token=api_token)
 
     body = '# Test\nThis page is created automatically.'
-    res = create_page(base_url, api_token, '/test/hoge', body)
+    res = growi.create_page('/test/hoge', body)
     pprint.pprint(res)
 
     body = '# Test\nThis page is created automatically :tada:  \nThis page is created automatically.  \nThis page is created automatically.'
-    res = update_page(base_url, api_token, '/test/hoge', body)
+    res = growi.update_page('/test/hoge', body)
     pprint.pprint(res)
 
-    res = rename_page(base_url, api_token, '/test/hoge', '/test/hoge3')
+    res = growi.rename_page('/test/hoge', '/test/hoge2')
     pprint.pprint(res)
 
-    res = get_tag_list(base_url, api_token)
+    res = growi.get_tag_list()
     pprint.pprint(res)
 
-    res = get_tags_by_page(base_url, api_token, "/test/hoge3")
+    res = growi.get_tags_by_page("/test/hoge2")
     pprint.pprint(res)
 
-    res = get_page_info(base_url, api_token, '/test/hoge3')
+    res = growi.get_page_info('/test/hoge2')
     pprint.pprint(res)
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 
 import json
 import requests
-from typing import Union
+from typing import Dict, List, Union
 
 
 # Growi Rest API向けのError
@@ -25,228 +25,235 @@ class GrowiAPIError(Exception):
             return str(self.description)
 
 
-##### Get Information #####
+class GrowiAPI(object):
+    """Abstraction layer for GROWI API v3"""
 
-# 正常に動作することを確認できていない
-def exist_page(base_url: str, api_token: str, page_path: str) -> bool:
-    """
-    [Experiment] check existance of the spcified page
-    """
-    try:
-        res = get_page_info(base_url, api_token, page_path)
-        return res.get("page") is not None
-    except GrowiAPIError as e:
-        if e.status_code == 404:
-            return False
+    def __init__(self, base_url: str, api_token: str):
+        super().__init__()
+        self.base_url = base_url
+        self.api_token = api_token
+
+    def exist_page(self, page_path: str) -> bool:
+        """
+        [Experiment] check existance of the spcified page
+        """
+        try:
+            res = self.get_page_info(page_path)
+            return res.get("page") is not None
+        except GrowiAPIError as e:
+            if e.status_code == 404:
+                return False
+            else:
+                raise e
+
+        # if res.status_code == 200:
+        #     return json.loads(res.text)['ok']
+        # else:
+        #     raise GrowiAPIError(res.text)
+
+
+    def get_page_info(self, page_path: str) -> dict:
+        """
+        get page information
+        """
+        req_url = '{}{}'.format(self.base_url, '/_api/v3/page')
+        params={'access_token': f'{self.api_token}', 'path': page_path}
+        res = requests.get(req_url, params=params)
+
+        if res.status_code == 200:
+            return json.loads(res.text)
         else:
-            raise e
-
-    # if res.status_code == 200:
-    #     return json.loads(res.text)['ok']
-    # else:
-    #     raise GrowiAPIError(res.text)
+            raise GrowiAPIError(res.text, res.status_code)
 
 
-def get_page_info(base_url: str, api_token: str, page_path: str) -> dict:
+    def get_page_list_by_page(self, page_path: str, limit:Union[int, None]=None) -> dict:
+        """
+        get page list under the specified page path
+        """
+        req_url = '{}{}'.format(self.base_url, '/_api/pages.list')
+        limit = 1e10 if limit is None else limit
+        queries = {
+            'access_token': f'{self.api_token}',
+            'path': page_path,
+            'limit': limit,
+        }
+        res = requests.get(req_url, params=queries)
+        if res.status_code == 200:
+            return json.loads(res.text)['pages']
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
+
+
+    def get_page_list_by_user(self, user: str, limit:Union[int, None]=None) -> dict:
+        """
+        get page list owned by specified user
+        """
+        req_url = '{}{}'.format(self.base_url, '/_api/pages.list')
+        limit = 1e10 if limit is None else limit
+        queries = {
+            'access_token': f'{self.api_token}',
+            'user': user,
+            'limit': limit,
+        }
+        res = requests.get(req_url, params=queries)
+        if res.status_code == 200:
+            return json.loads(res.text)['pages']
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
+
+
+    ##### Create Page #####
+
+    def create_page(self, page_path: str, body: str, grant: int=1) -> dict:
+        """
+        create page
+        """
+        req_url = '{}{}'.format(self.base_url, '/_api/v3/pages')
+        params = {
+            'access_token': f'{self.api_token}',
+        }
+
+        payloads = {
+            'path': page_path,
+            'body': body,
+            'grant': grant,
+        }
+
+        res = requests.post(req_url, data=payloads, params=params)
+        if res.status_code == 201:
+            return json.loads(res.text)
+        elif res.status_code == 400:
+            raise GrowiAPIError(f'Page already exists ({res.text})', res.status_code)
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
+
+
+    def update_page(self, page_path: str, body: str, grant: int=1) -> dict:
+        """
+        update page
+        """
+        res = self.get_page_info(page_path) # ページの存在しない場合get_page_infoはstatus_code=404で失敗し、get_page_infoが例外を投げる
+
+        page_info = res['page']
+        page_id = page_info['_id']
+        revision_id = page_info['revision']['_id']
+
+        req_url = '{}{}'.format(self.base_url, '/_api/pages.update')
+        params = {
+            'access_token': f'{self.api_token}',
+        }
+
+        payloads = {
+            'path': page_path,
+            'body': body,
+            'page_id': str(page_id),
+            'revision_id': str(revision_id),
+            'grant': grant,
+        }
+
+        res = requests.post(req_url, data=payloads, params=params)
+        if res.status_code == 200:
+            return json.loads(res.text)
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
+
+
+    #### Delete Page #####
+
+    # ページの削除はRestAPIではできない模様
+    # 厳密にはログイン状態を維持した状態でRest APIを叩かないと削除できない(Seleniumやbeautifusoupを駆使すれば削除は可能)
     """
-    get page information
+    def delete_page(self, page_path: str, recursively: bool=True, completely: bool=True) -> dict:
+        page_info = self.get_page_info(page_path)
+        page_id = page_info['_id']
+        revision_id = page_info['revision']['_id']
+
+        req_url = '{}{}'.format(self.base_url, '/_api/pages.remove')
+        params = {
+            'access_token': f'{self.api_token}',
+        }
+
+        payloads = {
+            'page_id': str(page_id),
+            'revision_id': str(revision_id),
+            'recursively': recursively,
+            'completely': completely,
+        }
+
+        res = requests.post(req_url, data=payloads, params=params)
+        if res.status_code == 200:
+            return json.loads(res.text)
+        else:
+            raise GrowiAPIError(res.text)
     """
-    req_url = '{}{}'.format(base_url, '/_api/v3/page')
-    params={'access_token': f'{api_token}', 'path': page_path}
-    res = requests.get(req_url, params=params)
-
-    if res.status_code == 200:
-        return json.loads(res.text)
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
 
 
-def get_page_list_by_page(base_url: str, api_token: str, page_path: str, limit:Union[int, None]=None) -> dict:
-    """
-    get page list under the specified page path
-    """
-    req_url = '{}{}'.format(base_url, '/_api/pages.list')
-    limit = 1e10 if limit is None else limit
-    queries = {
-        'access_token': f'{api_token}',
-        'path': page_path,
-        'limit': limit,
-    }
-    res = requests.get(req_url, params=queries)
-    if res.status_code == 200:
-        return json.loads(res.text)['pages']
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
+    ##### Rename Page #####
+
+    def rename_page(self, src_page_path: str, target_page_path: str, is_remain_meta_data: bool=True) -> dict:
+        """
+        rename page (change page path)
+        """
+        res = self.get_page_info(src_page_path)
+        
+        page_info = res['page']
+        page_id = page_info['_id']
+        revision_id = page_info['revision']['_id']
+
+        req_url = '{}{}'.format(self.base_url, '/_api/v3/pages/rename')
+        params = {
+            'access_token': f'{self.api_token}',
+        }
+
+        # all queries required
+        payloads = {
+            'pageId': page_id,
+            'revisionId': revision_id,
+            'path': src_page_path,
+            'newPagePath': target_page_path,
+            'isRemainMetadata': 'true' if is_remain_meta_data else 'false',
+        }
+
+        res = requests.put(req_url, data=payloads, params=params)
+        if res.status_code == 200:
+            return json.loads(res.text)
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
 
 
-def get_page_list_by_user(base_url: str, api_token: str, user: str, limit:Union[int, None]=None) -> dict:
-    """
-    get page list owned by specified user
-    """
-    req_url = '{}{}'.format(base_url, '/_api/pages.list')
-    limit = 1e10 if limit is None else limit
-    queries = {
-        'access_token': f'{api_token}',
-        'user': user,
-        'limit': limit,
-    }
-    res = requests.get(req_url, params=queries)
-    if res.status_code == 200:
-        return json.loads(res.text)['pages']
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
+    ##### Get Tag #####
 
+    def get_tag_list(self) -> Dict:
+        """
+        get tag list
+        """
+        req_url = '{}{}'.format(self.base_url, '/_api/tags.list')
+        params = {
+            'access_token': f'{self.api_token}',
+        }
 
-##### Create Page #####
+        res = requests.get(req_url, params=params)
+        if res.status_code == 200:
+            return json.loads(res.text)['data']
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
 
-def create_page(base_url: str, api_token: str, page_path: str, body: str, grant: int=1) -> dict:
-    """
-    create page
-    """
-    req_url = '{}{}'.format(base_url, '/_api/v3/pages')
-    params = {
-        'access_token': f'{api_token}',
-    }
+    def get_tags_by_page(self, page_path: str) -> List[str]:
+        """
+        get tags annotated on specified pate
+        """
+        res = self.get_page_info(page_path)
+        
+        page_id = res['page']['_id']
 
-    payloads = {
-        'path': page_path,
-        'body': body,
-        'grant': grant,
-    }
+        req_url = '{}{}'.format(self.base_url, '/_api/pages.getPageTag')
+        params = {
+            'access_token': f'{self.api_token}',
+            'pageId': page_id
+        }
 
-    res = requests.post(req_url, data=payloads, params=params)
-    if res.status_code == 201:
-        return json.loads(res.text)
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
-
-
-def update_page(base_url: str, api_token: str, page_path: str, body: str, grant: int=1) -> dict:
-    """
-    update page
-    """
-    res = get_page_info(base_url, api_token, page_path) # ページの存在しない場合get_page_infoはstatus_code=404で失敗し、get_page_infoが例外を投げる
-
-    page_info = res['page']
-    page_id = page_info['_id']
-    revision_id = page_info['revision']['_id']
-
-    req_url = '{}{}'.format(base_url, '/_api/pages.update')
-    params = {
-        'access_token': f'{api_token}',
-    }
-
-    payloads = {
-        'path': page_path,
-        'body': body,
-        'page_id': str(page_id),
-        'revision_id': str(revision_id),
-        'grant': grant,
-    }
-
-    res = requests.post(req_url, data=payloads, params=params)
-    if res.status_code == 200:
-        return json.loads(res.text)
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
-
-
-#### Delete Page #####
-
-# ページの削除はRestAPIではできない模様
-# 厳密にはログイン状態を維持した状態でRest APIを叩かないと削除できない(Seleniumやbeautifusoupを駆使すれば削除は可能)
-"""
-def delete_page(base_url: str, api_token: str, page_path: str, recursively: bool=True, completely: bool=True) -> dict:
-    page_info = get_page_info(base_url, api_token, page_path)
-    page_id = page_info['_id']
-    revision_id = page_info['revision']['_id']
-
-    req_url = '{}{}'.format(base_url, '/_api/pages.remove')
-    params = {
-        'access_token': f'{api_token}',
-    }
-
-    payloads = {
-        'page_id': str(page_id),
-        'revision_id': str(revision_id),
-        'recursively': recursively,
-        'completely': completely,
-    }
-
-    res = requests.post(req_url, data=payloads, params=params)
-    if res.status_code == 200:
-        return json.loads(res.text)
-    else:
-        raise GrowiAPIError(res.text)
-"""
-
-
-##### Rename Page #####
-
-def rename_page(base_url: str, api_token: str, src_page_path: str, target_page_path: str, is_remain_meta_data: bool=True) -> dict:
-    """
-    rename page (change page path)
-    """
-    res = get_page_info(base_url, api_token, src_page_path)
-    
-    page_info = res['page']
-    page_id = page_info['_id']
-    revision_id = page_info['revision']['_id']
-
-    req_url = '{}{}'.format(base_url, '/_api/v3/pages/rename')
-    params = {
-        'access_token': f'{api_token}',
-    }
-
-    # all queries required
-    payloads = {
-        'pageId': page_id,
-        'revisionId': revision_id,
-        'path': src_page_path,
-        'newPagePath': target_page_path,
-        'isRemainMetadata': 'true' if is_remain_meta_data else 'false',
-    }
-
-    res = requests.put(req_url, data=payloads, params=params)
-    if res.status_code == 200:
-        return json.loads(res.text)
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
-
-
-##### Get Tag #####
-
-def get_tag_list(base_url: str, api_token: str) -> dict:
-    """
-    get tag list
-    """
-    req_url = '{}{}'.format(base_url, '/_api/tags.list')
-    params = {
-        'access_token': f'{api_token}',
-    }
-
-    res = requests.get(req_url, params=params)
-    if res.status_code == 200:
-        return json.loads(res.text)['data']
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
-
-def get_tags_by_page(base_url: str, api_token: str, page_path: str) -> list:
-    """
-    get tags annotated on specified pate
-    """
-    res = get_page_info(base_url, api_token, page_path)
-    
-    page_id = res['page']['_id']
-
-    req_url = '{}{}'.format(base_url, '/_api/pages.getPageTag')
-    params = {
-        'access_token': f'{api_token}',
-        'pageId': page_id
-    }
-
-    res = requests.get(req_url, params=params)
-    if res.status_code == 200:
-        return json.loads(res.text)['tags']
-    else:
-        raise GrowiAPIError(res.text, res.status_code)
+        res = requests.get(req_url, params=params)
+        if res.status_code == 200:
+            return json.loads(res.text)['tags']
+        else:
+            raise GrowiAPIError(res.text, res.status_code)
